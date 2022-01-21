e9c34ec9daf3513a18fd2ee38cc8f327bb3896f0
梅丹隆 <meidanlong@maoyan.com>
2022-01-21 15:13:35 +0800

    添加追加报错方法

--- src/main/java/com/wtrue/rical/common/utils/validate/BaseValid.java
+++ src/main/java/com/wtrue/rical/common/utils/validate/BaseValid.java
@@ -15,41 +15,95 @@ class BaseValid extends BaseObject {
     private BaseError error = new BaseError();
 
 
+    /**
+     * 校验参数是否合法
+     * @param valid
+     */
     public void setValid(boolean valid) {
         this.valid = valid;
     }
 
+    /**
+     * 设置校验参数是否合法
+     * @return
+     */
     public boolean isValid() {
         return valid;
     }
 
+    /**
+     * 填充错误信息
+     * @param error
+     */
     public void setError(BaseError error) {
         this.setValid(false);
         this.error = error;
     }
 
+    /**
+     * 填充错误信息
+     * @param code
+     * @param message
+     */
     public void setError(Integer code, String message) {
         this.setValid(false);
         this.error.setCode(code);
         this.error.setMessage(message);
     }
 
+    /**
+     * 填充错误信息
+     * @param message
+     * @param params
+     */
     public void setError(String message, String... params){
         message = String.format(message, params);
-        this.setValid(false);
-        this.error.setCode(ErrorEnum.PARAM_ERROR.getCode());
-        this.error.setMessage(message);
+        setError(ErrorEnum.PARAM_ERROR.getCode(), message);
     }
 
+    /**
+     * 追加报错信息，如校验之前已有报错则不再填充错误信息
+     * @param message
+     * @param params
+     */
+    public void setFollowError(String message, String... params){
+        message = String.format(message, params);
+        setFollowError(ErrorEnum.PARAM_ERROR.getCode(), message);
+    }
+
+    /**
+     * 追加报错信息，如校验之前已有报错则不再填充错误信息
+     * @param code
+     * @param message
+     */
+    public void setFollowError(Integer code, String message){
+        if(!valid){
+            setError(code, message);
+        }
+    }
+
+    /**
+     * 填充错误信息，并抛出异常
+     * @param message
+     * @param params
+     */
     public void throwError(String message, String... params){
         setError(message, params);
         throw new VException(error.getMessage());
     }
 
+    /**
+     * 获取错误对象
+     * @return
+     */
     public BaseError getError() {
         return error;
     }
 
+    /**
+     * 获取错误信息
+     * @return
+     */
     public String getMessage(){
         return this.error.getMessage();
     }
