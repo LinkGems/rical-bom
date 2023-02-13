e44da66bb8efc5ae0da96a5d8c365f6e4b5348ee
luzelong <382491212@qq.com>
2023-02-13 20:32:13 +0800

    完善夏娃

---
+++ adam/src/main/java/com/wtrue/rical/common/adam/exception/ValidateException.java
@@ -0,0 +1,17 @@
+package com.wtrue.rical.common.adam.exception;
+
+import com.wtrue.rical.common.adam.enums.ErrorEnum;
+
+/**
+ * @description:
+ * @author: meidanlong
+ * @date: 2022/1/23 7:28 PM
+ */
+public class ValidateException extends BusinessException {
+    public ValidateException(String message) {
+        super(ErrorEnum.PARAM_ERROR, message);
+    }
+    public ValidateException(String message, String... params) {
+        super(ErrorEnum.PARAM_ERROR, String.format(message, params));
+    }
+}
