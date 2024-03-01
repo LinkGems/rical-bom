c4653cd3473ade7dba4be2d882f5e4dcba372e14
meidanlong <meidanlong@maoyan.com>
2024-03-01 09:37:49 +0800

    fastjson替换为jackson

--- eve/src/main/java/org/linkgems/rical/common/eve/controller/SelfCheckController.java
+++ eve/src/main/java/org/linkgems/rical/common/eve/controller/SelfCheckController.java
@@ -1,10 +1,10 @@
 package org.linkgems.rical.common.eve.controller;
 
-import com.alibaba.fastjson.JSON;
 import org.linkgems.rical.common.eve.domain.SelfCheckRequest;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.ArrayUtils;
+import org.linkgems.rical.common.eve.utils.JacksonUtil;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
@@ -68,7 +68,7 @@ public class SelfCheckController implements BeanFactoryAware {
             Method method = beanClazz.getMethod(request.getMethodName(), parameterTyps);
             Object returnValue = method.invoke(targetBean, request.getParameters());
 
-            return JSON.toJSONString(returnValue);
+            return JacksonUtil.writeValueAsString(returnValue);
 
         } catch (Exception e) {
             log.error("errMsg : {}", e.getMessage(), e);
